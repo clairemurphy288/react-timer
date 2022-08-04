4ca2f94a8b1d80ba9d37bd4f985b5192c6c6e22c
Claire <murphyclaire91@gmail.com>
2022-08-04 11:31:24 -0400

    timer functionality working!!!

--- src/components/timer.js
+++ src/components/timer.js
@@ -3,26 +3,35 @@ import React, {Component} from 'react';
 import "./timer.css"
 import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
 export default function Timer(props) { 
-    const [date, setDate] = useState(Date.now());
-    const [task, setTask] = useState(0);
+    const [date, setDate] = useState(new Date().toLocaleString());
+    const [initialTime, setInitialTime] = useState(0);
     const [hours, setHours] = useState(0);
     const [minutes, setMinutes] = useState(0);
     const [seconds, setSeconds] = useState(0);
     function startTimer(e) {
+        const w = Date.now();
+        setInitialTime(w);
         console.log("timer started");
-        const date = new Date().toLocaleString();
-        console.log(date)
-        const dateInMili = Date.parse(date)
-        console.log(dateInMili);
-
     }
+    useEffect(()=> {
+        if(initialTime !== 0) {
+            setInterval(()=> {
+                const currentTime = Date.now();
+                const delta = currentTime  - initialTime;
+                setDate(msToTime(delta));
+    
+            }, 1000)
+
+        }
+
+    },[initialTime])
 
     return (
         <div onClick={startTimer} className="body">
             <button className="btn btn-dark ">Start Timer</button>
             <div className="container1">
                 <div className = "timer-box">
-                    <h1 id="timer">{`${hours}:${minutes}:${seconds}` }</h1>
+                    <h1 id="timer">{`${date}` }</h1>
                     
                 </div>
                 
@@ -30,4 +39,16 @@ export default function Timer(props) {
         </div>);
 
 }
+function msToTime(duration) {
+    // const milliseconds = parseInt((duration % 1000) / 100);
+    var seconds = Math.floor((duration / 1000) % 60);
+    var minutes = Math.floor((duration / (1000 * 60)) % 60);
+    var hours = Math.floor((duration / (1000 * 60 * 60)) % 24);
+    
+    hours = (hours < 10) ? "0" + hours : hours;
+    minutes = (minutes < 10) ? "0" + minutes : minutes;
+    seconds = (seconds < 10) ? "0" + seconds : seconds;
+  
+    return hours + ":" + minutes + ":" + seconds 
+  }
  
